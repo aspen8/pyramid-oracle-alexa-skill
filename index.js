47ed48da5224e6dd857882fa36a52743b0b1eb78
aspen8 <alasdair.rae@gmail.com>
2017-09-28 12:21:39 +0100

    demo1

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var Alexa = require('alexa-sdk');
 //=========================================================================================================================================
 //TODO: The items below this comment need your attention.
 //=========================================================================================================================================
-//
+
 //Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.  
 //Make sure to enclose your value in quotes, like this: var APP_ID = "amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1";
 var APP_ID = undefined;
